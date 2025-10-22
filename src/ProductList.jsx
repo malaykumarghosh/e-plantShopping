93a7140d1592583dcc88e331a2538a8855fae229
Malay Ghosh <mail.malay@gmail.com>
2025-10-22 13:15:15 -0400

    remaining cart functionality added

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -262,6 +262,9 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (plant) => {
+        if(addedToCart[plant.name] !== undefined)
+            return;
+
         dispatch(addItem(plant)); // Dispatch the action to add the product/plant to the cart (Redux action) 
         
         setAddedToCart((prevState) => ({ // Update the local state to reflect that the product/plant has been added
@@ -289,7 +292,7 @@ function ProductList({ onHomeClick }) {
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
-                    <text id="cart-count" x="125" y="150" text-anchor="middle" font-size="60" style="fill:white">{cart_count}</text>
+                    <text id="cart-count" x="125" y="150" text-anchor="middle" font-size="60" fill="white">{cart_count}</text>
                     </svg></h1></a></div>
                 </div>
             </div>
@@ -298,7 +301,7 @@ function ProductList({ onHomeClick }) {
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                     <div key={index}> {/* Unique key for each category div */}
                         <h1>
-                        <div>{category.category}</div> {/* Display the category name */}
+                        <div className='plantname_heading'><div className='plant_heading'>{category.category}</div></div> {/* Display the category name */}
                         </h1>
                         <div className="product-list"> {/* Container for the list of plant cards */}
                         {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
@@ -311,12 +314,12 @@ function ProductList({ onHomeClick }) {
                             <div className="product-title">{plant.name}</div> {/* Display plant name */}
                             {/* Display other plant details like description and cost */}
                             <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                            <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                            <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                             <button
-                                className="product-button"
+                                className={addedToCart[plant.name] !== undefined ?  "product-button added-to-cart" : "product-button"}
                                 onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                             >
-                                Add to Cart
+                            {addedToCart[plant.name] !== undefined ? 'Added' : 'Add' } to Cart
                             </button>
                             </div>
                         ))}
